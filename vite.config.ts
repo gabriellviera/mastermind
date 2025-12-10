be73e19be3bfa419609e3537ba87f36247f16183
gabriellviera <gabriellviera@example.com>
2025-12-10 19:12:31 +0000

    Configure for root domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base removed to default to root '/' for custom domain support
 })
